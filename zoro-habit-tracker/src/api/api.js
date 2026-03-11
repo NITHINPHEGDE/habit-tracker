5c217c939b0cecc47cc9f2ed2747d71f8d70b41f
nithin p hegde <nithinphegde@gmail.com>
2026-03-11 17:10:48 +0530

    updated backedn url for deployment

--- zoro-habit-tracker/src/api/api.js
+++ zoro-habit-tracker/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-baseURL: "http://localhost:5000/api"
+baseURL: "https://habit-tracker-812p.onrender.com/api"
 })
 
 API.interceptors.request.use((req)=>{
